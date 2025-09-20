9965570f2f25163945c3734a0a8de04c48a50af2
Cahyadi240105 <cahyadiprasetyo659@gmail.com>
2025-09-20 14:24:24 +0700

    Tambah Folder

--- views/mahasiswa_create.php
+++ views/mahasiswa_create.php
@@ -1,8 +1,9 @@
 <h2>Tambah Mahasiswa</h2>
 <form method="POST">
-    Nama: <input type="text" name="nama"><br>
-    NIM: <input type="text" name="nim"><br>
-    Jurusan: <input type="text" name="jurusan"><br>
+    Nama: <input type="text" name="nama" required><br>
+    NIM: <input type="text" name="nim" required><br>
+    Jurusan: <input type="text" name="jurusan" required><br>
     <button type="submit">Simpan</button>
 </form>
 <a href="index.php?controller=mahasiswa&action=index">Kembali</a>
+
