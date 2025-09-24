e2c5ea8e2850fa129d9f6e2a86e5960a993b9f39
Cahyadi240105 <cahyadiprasetyo659@gmail.com>
2025-09-24 15:31:04 +0700

    Menambahkan icon pada list mahasigma

--- views/mahasiswa_list.php
+++ views/mahasiswa_list.php
@@ -1,7 +1,7 @@
 <?php include 'partials/header.php'; ?>
 
 <h2>Data Mahasiswa</h2>
-<a href="index.php?controller=mahasiswa&action=create">+ Tambah Mahasiswa</a>
+<a href="index.php?controller=mahasiswa&action=create"><i class="fa-solid fa-user-plus"></i> Tambah Mahasiswa</a>
 <table border="1" cellpadding="5" cellspacing="0">
     <tr>
         <th>ID</th>
@@ -13,18 +13,18 @@
         <th>Aksi</th>
     </tr>
     <?php foreach ($data as $m): ?>
-    <tr>
-        <td><?= htmlspecialchars($m['id']) ?></td>
-        <td><?= htmlspecialchars($m['nama']) ?></td>
-        <td><?= htmlspecialchars($m['nim']) ?></td>
-        <td><?= htmlspecialchars($m['email'] ?? '') ?></td>
-        <td><?= htmlspecialchars($m['fakultas']) ?></td>
-        <td><?= htmlspecialchars($m['jurusan'] ?? '') ?></td>
-        <td>
-            <a href="index.php?controller=mahasiswa&action=edit&id=<?= $m['id'] ?>">Edit</a> | 
-            <a href="index.php?controller=mahasiswa&action=delete&id=<?= $m['id'] ?>" onclick="return confirm('Hapus data?')">Delete</a>
-        </td>
-    </tr>
+        <tr>
+            <td><?= htmlspecialchars($m['id']) ?></td>
+            <td><?= htmlspecialchars($m['nama']) ?></td>
+            <td><?= htmlspecialchars($m['nim']) ?></td>
+            <td><?= htmlspecialchars($m['email'] ?? '') ?></td>
+            <td><?= htmlspecialchars($m['fakultas']) ?></td>
+            <td><?= htmlspecialchars($m['jurusan'] ?? '') ?></td>
+            <td>
+                <a href="index.php?controller=mahasiswa&action=edit&id=<?= $m['id'] ?>"><i class="fa-solid fa-user-pen"></i> Edit</a> |
+                <a href="index.php?controller=mahasiswa&action=delete&id=<?= $m['id'] ?>" onclick="return confirm('Hapus data?')"><i class="fa-solid fa-trash"></i>Delete</a>
+            </td>
+        </tr>
     <?php endforeach; ?>
 </table>
 
--- views/partials/header.php
+++ views/partials/header.php
@@ -1,9 +1,13 @@
 <!DOCTYPE html>
 <html lang="id">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= htmlspecialchars($title) ?></title>
     <link rel="stylesheet" href="<?= BASEURL  ?>assets/css/styles.css">
+    <!-- Latest Font Awesome (v6) -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" rel="stylesheet">
 </head>
+
 <body>
